7eae90e3f7439a97cba2398784ff9242306574ad
fgamester <falvarez.munoz@hotmail.es>
2024-12-25 13:46:49 -0300

    primitive hit and miss mechanich added

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cell from './components/Cell'
 
 const board = new Array(100).fill(null);
 
 const Board = () => {
+    const [remainPositions, setRemainPositions] = useState([31, 32, 33])
+
     return (
         <div id='game-board'>
-            {board.map((_, index) => <Cell key={index} />)}
+            {board.map((_, index) => <Cell key={index} position={index + 1} targets={remainPositions} hit={setRemainPositions} />)}
         </div>
     )
 }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState, memo, useCallback } from 'react'
 
-const Cell = () => {
-  return (
-    <div className='game-cell'>
-        
-    </div>
-  )
-}
+const Cell = memo(({ position, targets, hit }) => {
+    const [attacked, setAttacked] = useState(false);
+    const [targetAttacked, setTargetAttacked] = useState('')
 
-export default Cell
+    const fire = useCallback(() => {
+        if (!attacked) {
+            setAttacked(_ => true);
+            if (targets.some(item => item == position)) {
+                hit(prev => prev.filter(item => item != position));
+                setTargetAttacked(() => 'ship-hit');
+            } else {
+                setTargetAttacked(() => 'missed');
+            }
+            console.log(position);
+        }
+    }, [attacked]);
+
+    return (
+        <div onClick={fire} className={`game-cell ${!attacked && 'water'} ${targetAttacked}`}>
+
+        </div>
+    )
+});
+
+export default Cell;
